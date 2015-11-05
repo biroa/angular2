8eeba8513c2704e44deccf07c8dfbef560115f0d
biroa <adam.biro@gmail.com>
2015-11-05 22:41:16 +0000

    events with $event

---
+++ lessons/3/todoInput.js
@@ -0,0 +1,11 @@
+import {Component} from "angular2/angular2";
+
+@Component({
+    selector: 'todo-input',
+    template: `<input type="text" #log-me><button (click)="onClick($event,logMe.value)">Log Input</button>`
+})
+export class TodoInput{
+    onClick(event,value){
+        console.log(event, value);
+    }
+}
