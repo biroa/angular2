ab43f8c25743521de68387a1be650eb509c0509e
biroa <adam.biro@gmail.com>
2015-11-11 23:14:58 +0000

    js files

---
+++ lessons/4/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by biroa on 11/11/2015.
+ */
+import {bootstrap, Component} from "angular2/angular2";
+import {TodoInput} from "./todoInput";
+import {TodoService} from "./todoService";
+
+@Component({
+    selector:'app',
+    directives: [TodoInput],
+    template: `
+        <div><todo-input></todo-input></div>
+    `
+})
+class App{}
+
+bootstrap(App, [TodoService]);
---
+++ lessons/4/todoInput.js
@@ -0,0 +1,25 @@
+/**
+ * Created by biroa on 11/11/2015.
+ */
+import {Component} from "angular2/angular2";
+import {TodoService} from "./todoService";
+@Component({
+    selector: 'todo-input',
+    template: `
+        <input type="text" #log-me>
+        <button on-click="onClick($event, logMe.value)">Log Input</button>
+    `
+})
+export class TodoInput{
+
+    constructor(
+        public todoService:TodoService
+){
+    console.log(todoService);
+}
+
+onClick(event, value){
+    this.todoService.addTodo(value);
+    console.log(this.todoService.todos);
+}
+}
